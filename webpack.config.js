a406cd5ac4c82a3c8228658eb92623d016d92c48
Vit Gottwald <vit.gottwald@gmail.com>
2018-06-14 00:21:32 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const pathResolve = aPath => path.resolve(__dirname, aPath);
+
+module.exports = {
+  mode: "development",
+  entry: pathResolve("index.js"),
+  output: {
+    path: pathResolve("dist"),
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
