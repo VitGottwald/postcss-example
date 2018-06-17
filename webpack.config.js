47eaa5734805b6e9868dfab7c0b70ea9019b8dbb
Vit Gottwald <vit.gottwald@gmail.com>
2018-06-17 21:27:53 +0200

    Move sources to `src` folder and `index.html` to `dist`

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const pathResolve = aPath => path.resolve(__dirname, aPath);
 
 module.exports = {
-  mode: "development",
-  entry: pathResolve("index.js"),
+  mode: "production",
+  entry: pathResolve("./src/index.js"),
   output: {
     path: pathResolve("dist"),
     filename: "main.js"
